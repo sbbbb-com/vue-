3ab88e99040c956b646e40a61126ab1b14c4faa9
sbbbb-com <17719357702@163.com>
2021-08-29 16:12:18 +0800

    忘了 提交了，，，webpack使用5  创建 本地服务器

---
+++ stu03-day4/01-webpack的使用/05-webpack配置vue-插件/src/vue/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2 class="title">{{message}}</h2>
+    <button @click="btnClick">按钮</button>
+    <h2>{{name}}</h2>
+    <!--使用组件-->
+    <Cpn></Cpn>
+  </div>
+</template>
+
+<script>
+import Cpn from "./Cpn.vue";
+
+
+export default {
+  name: "App",
+  components: {
+    Cpn //注册进cpn
+  },
+  data(){
+    return　{
+      message: "Hello Webpack",
+      name: "测试人员"
+    }
+  },
+  methods: {
+    btnClick(){
+      console.log("btnClick");
+    }
+  }
+}
+</script>
+<style scoped>
+  .title{
+    color: green;
+}
+</style>
