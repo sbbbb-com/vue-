ab61acedc636544eb72c06a2e551d1eb731bc4ab
sbbbb-com <17719357702@163.com>
2021-10-03 16:46:51 +0800

    home页面的整体布局实现

--- de1/src/components/Home.vue
+++ de1/src/components/Home.vue
@@ -2,7 +2,11 @@
   <!--home组件-->
   <el-container class="home-container">
     <!-- 头部区域-->
-    <el-header>Header
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
       <el-button type="info" @click="logout">退出</el-button>
     </el-header>
     <!--页面主体区域-->
@@ -36,6 +40,19 @@ export default {
 
 .el-header{
   background-color:#373d3f ;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: white;
+  font-size: 20px;
+  >div{
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
 }
 .el-aside{
   background-color: #323743;
