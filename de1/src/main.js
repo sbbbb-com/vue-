484bc3fa8744d5b8cad9ec61e9f67b045388775b
sbbbb-com <17719357702@163.com>
2021-10-03 21:11:09 +0800

    为请求添加 拦截器 目的是在每一次发起的请求前，在请求头在添加Authorization 字段 对应的就是本地sessionStorage中保存的token

--- de1/src/main.js
+++ de1/src/main.js
@@ -12,6 +12,16 @@ import '@/assets/fonts/iconfont'
 import axios from "axios";
 //设置请求的根路径
 axios.defaults.baseURL="http://127.0.0.1:8888/api/private/v1/"
+
+//设置请求的拦截器
+axios.interceptors.request.use(config=>{  //config请求对象
+    //console.log(config);
+    //在返回config前 对其进行预处理 添加请求头Authorization 字段 其对应的就是token
+    config.headers.Authorization=window.sessionStorage.getItem("token");
+    //使用完后必须return config
+    return config
+})
+
 //将对象挂载到vue的全局对象上
 Vue.prototype.$http=axios
 
