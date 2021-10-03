722f0602d829cf53713d66ba5f951acc77a9f635
sbbbb-com <17719357702@163.com>
2021-10-03 09:23:02 +0800

    element导入优化

--- de1/src/components/Home.vue
+++ de1/src/components/Home.vue
@@ -1,15 +1,25 @@
 <template>
-<div>
-<!--home组件-->11home组件
-  <el-button type="info" @click="logout">退出</el-button>
-</div>
+  <!--home组件-->
+  <el-container>
+    <!-- 头部区域-->
+    <el-header>Header
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!--页面主体区域-->
+    <el-container>
+      <!-- 侧边栏-->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧主体-->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
   name: "Home",
   methods: {
-    logout(){
+    logout() {
       // 清除会话保存的 token
       window.sessionStorage.clear()
       //使用跳转到登陆页
