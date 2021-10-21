ebb2a150d84a39637111f283534d30b47b745659
sbbbb-com <17719357702@163.com>
2021-10-21 15:10:18 +0800

    获取用户列表数据

--- de1/src/components/user/Users.vue
+++ de1/src/components/user/Users.vue
@@ -28,7 +28,39 @@
 
 <script>
 export default {
-  name: "Users"
+  name: "Users",
+  data(){
+    return {
+      //定义获取用户列表的请求的get参数
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 2
+      },
+      userList: [],
+      total : 0
+    }
+  },
+  created() {
+    //发起请求
+    this.getUserList();
+  },
+  methods: {
+    //获取用户列表
+    async getUserList(){
+      const {data:res}=await this.$http.get('users',{
+        params: this.queryInfo
+      });
+      if (res.meta.status!==200) {
+        return this.$message.error("获取用户列表失败")
+      }
+      //获取到具体值保存到data中数组
+      this.userList=res.data.users;
+      this.total=res.data.total;
+      console.log(this.userList);
+      console.log(this.total);
+    }
+  }
 }
 </script>
 
