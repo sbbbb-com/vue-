7fe017f8d43ffbec42169bcae4fd93e782e582ec
sbbbb-com <17719357702@163.com>
2021-11-09 20:56:11 +0800

    一级权限的美化

--- de1/src/components/power/Roles.vue
+++ de1/src/components/power/Roles.vue
@@ -23,10 +23,21 @@
         <!-- 展开列 -->
         <el-table-column type="expand">
           <template slot-scope="scope">
-            <pre>
-              {{scope.row}}
-            </pre>
-
+            <!-- 页面布局 -->
+            <el-row
+              :class="['bdbottom',i1===0?'bdtop':'']"
+              v-for="(item1,i1) in scope.row.children"
+              :key="item1.id">
+              <!-- 渲染一级权限 -->
+              <el-col :span="5" >
+                <el-tag>{{item1.authName}}</el-tag>
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+              <!-- 渲染二级三级权限 -->
+              <el-col span="19">
+                {{i1}}
+              </el-col>
+            </el-row>
           </template>
         </el-table-column>
         <!-- 索引列 -->
@@ -76,5 +87,16 @@ export default {
 </script>
 
 <style scoped lang="less">
+.el-tag{
+  margin: 7px;
+}
+
+.bdtop{
+  border-top: 1px solid #eee;
+}
+
+.bdbottom{
+  border-bottom: 1px solid #eee;
+}
 
 </style>
