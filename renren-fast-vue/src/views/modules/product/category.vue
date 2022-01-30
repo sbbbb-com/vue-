82ab3bb5e8cce78591a72771b4464714db1f6869
one_one_root <180054421@qq.com>
2022-01-30 15:28:41 +0800

    菜单项的部分提交

--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -1,6 +1,31 @@
 <template>
   <div>
-    <el-tree :data="memus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+    <el-tree
+      :data="memus"
+      :props="defaultProps"
+      show-checkbox
+      node-key="catId"
+      :expand-on-click-node="false">
+       <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.childNodes.length>0"
+            type="text"
+            size="mini"
+            @click="() => append(data)">
+            添加
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length===0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)">
+            删除
+          </el-button>
+        </span>
+      </span>
+    </el-tree>
   </div>
 </template>
 
@@ -23,9 +48,6 @@ export default {
     this.getMenuData();
   },
   methods: {
-    handleNodeClick (data) {
-      console.log(data)
-    },
     //获取菜单数据
     getMenuData(){
       this.$http({
@@ -36,7 +58,40 @@ export default {
         // 设置菜单数据
         this.memus=data.data
       })
-    }
+    },
+    // 节点添加
+    append(data) {
+      console.log("点击添加按钮",data)
+    },
+    // 节点删除
+    remove(node, data) {
+      //在正式删除前进行弹窗提示：
+      this.$confirm(`是否删除【${data.name}】菜单？`, '警告！！', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+
+        let ids=[data.catId]
+        //console.log("点击删除按钮",node,data)
+        this.$http({
+          url: this.$http.adornUrl("/product/category/delete"),
+          method: "POST",
+          data: this.$http.adornData(ids,false)
+        }).then(({data})=>{
+          //console.log("删除成功...",data)
+          //删除后刷新数据
+          this.getMenuData();
+        })
+
+        //删除成功后提示消息
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        });
+      }).catch((er)=>{
+        })
+    },
   }
 }
 </script>
